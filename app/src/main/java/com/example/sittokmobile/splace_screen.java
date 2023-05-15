c5f95ec9bb18ed69593be03da6d54c3259806142
fertilian <fertiliast2002@gmail.com>
2023-05-15 11:22:38 +0700

    update

---
+++ app/src/main/java/com/example/sittokmobile/splace_screen.java
@@ -0,0 +1,14 @@
+package com.example.sittokmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splace_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splace_screen);
+    }
+}
