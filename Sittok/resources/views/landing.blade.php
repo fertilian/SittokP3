48a7cc68c6eaf932c798af213b951e4c9b38eb1a
Nizarr20 <nizarhanner07@gmail.com>
2023-06-08 15:10:06 +0700

    p

--- Sittok/resources/views/landing.blade.php
+++ Sittok/resources/views/landing.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link href="/assets/img/logo/sittok-gambar.png" rel="icon">
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
 
     <title>Sittok</title>
