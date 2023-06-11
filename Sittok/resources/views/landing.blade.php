815edf28762ebb6b151675681e7c62c45a94cc67
Nizarr20 <nizarhanner07@gmail.com>
2023-06-11 21:54:20 +0700

    landing

--- Sittok/resources/views/landing.blade.php
+++ Sittok/resources/views/landing.blade.php
@@ -19,6 +19,18 @@
 
     <link rel="stylesheet" href="landing/css/templatemo-softy-pinko.css">
 
+    <script>
+        $(document).ready(function(){       
+            $('.form-checkbox').click(function(){
+                if($(this).is(':checked')){
+                    $('.form-password').attr('type','text');
+                }else{
+                    $('.form-password').attr('type','password');
+                }
+            });
+        });
+    </script>
+
     </head>
     
     <body>
@@ -32,9 +44,6 @@
         </div>
     </div>  
     <!-- ***** Preloader End ***** -->
-    
-    
-   
                         <!-- ***** Logo End ***** -->
                     </nav>
                 </div>
@@ -51,8 +60,10 @@
             <div class="container">
                 <div class="row">
                     <div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-1">
-                        <h1> Selamat datang di website <strong>admin Sittok</strong></h1>
-                        <p>Sittok merupakan sistem informasi toko komputer yang menyediakan serta melayani penjualan Komputer, Laptop, Spare Part, dan Aksesoris untuk komputer kesayangan anda.</p>
+                    <img src="/assets/img/logo/sittok.png" width="150px">
+                        <h1> Selamat Datang di Website <strong>SITTOK</strong></h1>
+                        <p>Sittok merupakan sistem informasi toko komputer yang menyediakan serta melayani penjualan Komputer, Laptop, Spare Part, dan Aksesoris untuk komputer kesayangan.</p>
+                        
                         <a href="loginn" class="main-button-slider" class="active">LOGIN</a>
                     </div>
                 </div>
@@ -60,68 +71,6 @@
         </div>
     
             <!-- ***** Section Title End ***** -->
-
-
-                </div>
-                <!-- ***** Contact Text End ***** -->
-
-                <!-- ***** Contact Form Start ***** -->
-                <div class="col-lg-8 col-md-6 col-sm-12">
-                    <div class="contact-form">
-                        <form id="contact" action="" method="get">
-                          <div class="row">
-                            <div class="col-lg-6 col-md-12 col-sm-12">
-                              <fieldset>
-                                <input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="">
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-6 col-md-12 col-sm-12">
-                              <fieldset>
-                                <input name="email" type="email" class="form-control" id="email" placeholder="E-Mail Address" required="">
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-12">
-                              <fieldset>
-                                <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-12">
-                              <fieldset>
-                                <button type="submit" id="form-submit" class="main-button">Send Message</button>
-                              </fieldset>
-                            </div>
-                          </div>
-                        </form>
-                    </div>
-                </div>
-                <!-- ***** Contact Form End ***** -->
-            </div>
-        </div>
-    </section>
-    <!-- ***** Contact Us End ***** -->
-    
-    <!-- ***** Footer Start ***** -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12">
-                    <ul class="social">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
-                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p class="copyright">Copyright &copy; 2023 Sittok</p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    
     <!-- jQuery -->
     <script src="landing/js/jquery-2.1.0.min.js"></script>
 
